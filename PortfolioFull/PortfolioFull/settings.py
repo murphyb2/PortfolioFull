06d4363636a69bafd21e87d13ba682b5c6cefcbe
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2020-01-13 15:09:20 -0500

    Added folder structure for frontend

--- PortfolioFull/PortfolioFull/settings.py
+++ PortfolioFull/PortfolioFull/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'devPortfolio',
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
