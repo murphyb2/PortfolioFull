104478a5b24a9da2dc064ba7c1206f262292efc8
murphyb2 <32982028+murphyb2@users.noreply.github.com>
2020-01-21 18:21:01 -0500

    React connections are connected

--- PortfolioFull/PortfolioFull/urls.py
+++ PortfolioFull/PortfolioFull/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('', include('frontend.urls')),
+    path('', include('frontend.urls')),
     path('', include('backend.devPortfolio.urls')),
     # path('', include('accounts.urls')),
     # path('', include('hiking.urls')),
